ea094b3e315bc9be1cbc562b935439c831201593
Rafael-Reyes-73 <rafael.reyes.chj@gmail.com>
2025-05-16 18:18:57 -0600

    Variables

--- sintaxi.py
+++ sintaxi.py
@@ -6,3 +6,25 @@ de varias lineas
 """
 #2. strings
 print("0 Miedo")
+
+variable1 = "0 Miedo"
+print(len (variable1))
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5])
+
+#3. Variables
+
+x = "Rafael Reyes"
+x = 4
+x = 5.76
+print(x)
+
+x = int(3)
+y = float(3)
+z = str(3)
+
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z))
