0806552eb9b4e4929f372bfc0fd418a27cece5ff
Rafael-Reyes-73 <rafael.reyes.chj@gmail.com>
2025-05-23 21:34:04 -0600

    Nueva excepción Par y Impar
    Se modifico una nueva excepción por si el usuario deja en blanco el campo a llenar

--- paroimapr.py
+++ paroimapr.py
@@ -1,10 +1,18 @@
-try:
- numero = int(input ("Ingresa un numero: "))
+while True:
+    numero = input("Ingresa un número: ").strip()
 
- if numero%2:
-     print("EL NUMERO ES IMPAR")     
- else:
-     print("El NUMERO ES PAR")
-  
-except ValueError:
- print ("Error: Se ingreso un caracter")
+    if numero == "":
+        print("Error: No ingresaste nada.")
+        continue
+
+    if numero == "0":
+        break
+
+    try:
+        numero = int(numero)
+        if numero % 2:
+            print("El número es impar")
+        else:
+            print("El número es par")
+    except ValueError:
+        print("Error: Se ingresó un carácter no numérico.")
