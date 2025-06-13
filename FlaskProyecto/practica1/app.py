6dfc96dc9e75fb1a8b0ce4c53d89efc3741a3894
Rafael-Reyes-73 <rafael.reyes.chj@gmail.com>
2025-06-12 20:06:13 -0600

    Rutas
    -Tipos de Rutas
    
    #ruta simple
    #ruta con un parámetro
    #ruta try-catch
    #ruta doble
    #ruta POST

--- FlaskProyecto/practica1/app.py
+++ FlaskProyecto/practica1/app.py
@@ -1,9 +1,32 @@
 from flask import Flask
 app = Flask(__name__)
 
+#ruta simple
 @app.route('/')
 def home():
     return '¡Hola, Flask!'
 
+#ruta con un parámetro
+@app.route('/saludo/<nombre>')
+def saludo(nombre):
+    return f'¡Hola, {nombre}!'
+
+#ruta try-catch
+@app.errorhandler(404)
+def paginaNoE(e):
+    return 'Página no encontradota', 404
+
+#ruta doble
+@app.route('/usuario')
+@app.route('/usuaria')
+def dobleroute():
+    return 'Soy el mismo recurso del servidor'
+
+#ruta POST
+@app.route('/formulario', methods=['GET'])
+def formulario():
+    return 'Soy un formulario'
+
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
+    
