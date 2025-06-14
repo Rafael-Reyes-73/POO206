3cd5ddfccb7f012b7aecea0c16e31459a1fb2484
Rafael-Reyes-73 <rafael.reyes.chj@gmail.com>
2025-06-13 18:36:39 -0600

    Conexión y Excepciones de la BD
    Creamos la conexion de nuestra BD con sus respectivas excepciones(host, user, password, y bd no encontrada)

--- FlaskProyecto/practica1/app.py
+++ FlaskProyecto/practica1/app.py
@@ -1,5 +1,42 @@
-from flask import Flask
+from flask import Flask,jsonify
+from flask_mysqldb import MySQL
+import MySQLdb
+
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'ElMejorenBD'
+app.config['MYSQL_DB'] = 'dbflask'
+app.config['MYSQL_PORT'] = 3306 
+mysql = MySQL(app)
+
+#ruta para probar la conexión a la base de datos
+@app.route('/DBCheck')
+def DBCheck():
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("Select 1")
+        return jsonify({'status': 'ok','message': 'Conexion exitosa a la base de datos'}), 200
+    except MySQLdb.MySQLError  as e:
+        return jsonify({'status': 'error','message':str (e)}), 500
+    #excepcion de contraseña incorrecta
+    except MySQLdb.OperationalError as e:
+        return jsonify({'status': 'error','message': 'Error de conexión a la base de datos: ' + str(e)}), 500
+    #excepcion de base de datos no encontrada
+    except MySQLdb.DatabaseError as e:
+        return jsonify({'status': 'error','message': 'Base de datos no encontrada: ' + str(e)}), 500
+    #excepcion usurio incorrecto
+    except MySQLdb.ProgrammingError as e:
+        return jsonify({'status': 'error','message': 'Usuario o contraseña incorrectos: ' + str(e)}), 500
+    #excepcion host in9correcto
+    except MySQLdb.InterfaceError as e:
+        return jsonify({'status': 'error','message': 'Host incorrecto: ' + str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
+    
+    
+
 
 #ruta simple
 @app.route('/')
@@ -22,11 +59,11 @@ def paginaNoE(e):
 def dobleroute():
     return 'Soy el mismo recurso del servidor'
 
+@app.errorhandler(404)
+def metodonoP(e):
+    return 'Revisa el metodo de envio de tu ruta (GET o POST) !!!', 405
+
 #ruta POST
-@app.route('/formulario', methods=['GET'])
+@app.route('/formulario', methods=['POST'])
 def formulario():
     return 'Soy un formulario'
-
-if __name__ == '__main__':
-    app.run(port=3000, debug=True)
-    
